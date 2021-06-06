255d9fe510da546383d8e761e66ebc2ce1e26c2d
mengyuan <1647786154@qq.com>
2021-06-06 23:17:59 +0800

    多个任务进行调度

--- test/cronexpr_test.go
+++ test/cronexpr_test.go
@@ -38,3 +38,73 @@ func TestCronexprDemo1(t *testing.T) {
 	// fmt.Println(now)
 	// fmt.Println(nextTime)
 }
+
+/**
+ * 调度多个任务
+ */
+// 代表一个任务
+type CronJob struct {
+	expr *cronexpr.Expression
+	nextTime time.Time
+}
+func TestCronexprMoreJob(t *testing.T) {
+	var (
+		cronJob *CronJob
+		expr *cronexpr.Expression
+		now time.Time
+		scheduleTable map[string]*CronJob
+	)
+	// 当前时间
+	now = time.Now()
+	// 分配空间
+	scheduleTable = make(map[string]*CronJob)
+
+	// 定义两个cronjob
+	expr = cronexpr.MustParse("*/5 * * * * * *")
+	cronJob = &CronJob{
+		expr: expr,
+		nextTime: expr.Next(now),
+	}
+	// 将任务注册到调度表
+	scheduleTable["job1"] = cronJob
+
+	expr = cronexpr.MustParse("*/5 * * * * * *")
+	cronJob = &CronJob{
+		expr: expr,
+		nextTime: expr.Next(now),
+	}
+	// 将任务注册到调度表
+	scheduleTable["job2"] = cronJob
+
+	// 启动一个协程进行调度
+	go func() {
+		var (
+			jobName string
+			cronJob *CronJob
+			now time.Time
+		)
+		// 不停的检查调度表
+		for {
+			now = time.Now()
+			for jobName, cronJob = range scheduleTable {
+				// 判断是否过期
+				if cronJob.nextTime.Before(now) || cronJob.nextTime.Equal(now) {
+					// 启动协程执行任务
+					go func(jobName string) {
+						fmt.Println("执行调度", jobName)
+					}(jobName)
+					// 计算下次调度的时间
+					cronJob.nextTime = cronJob.expr.Next(now)
+					fmt.Println("计算出下次调度的时间是：", cronJob.nextTime)
+				}
+			}
+
+			// time.Sleep(100*time.Millisecond) // 睡眠100毫秒
+			select {
+			case <- time.NewTimer(100 * time.Millisecond).C:
+			}
+		}
+	}()
+
+	time.Sleep(100 * time.Second)
+}
